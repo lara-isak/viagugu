eb47a1b133115f0fffe81a17ebc8152390ecd3e1
Lara Isak <luckasti.larry@gmail.com>
2023-05-11 18:53:24 +0200

    💄 Center text on small screens as well

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black">
       
-      <div className="w-full max-w-5xl items-center justify-center lg:flex text-white">
+      <div className="w-full px-10 lg:max-w-5xl items-center justify-center lg:flex text-white">
         <p className="font-bangers text-8xl tracking-wider text-green-500">COMING SOON</p>
       </div>
 
