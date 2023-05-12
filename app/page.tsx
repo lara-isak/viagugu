81bff28c47458814dff5c8fa20b06fc821331a14
Lara Isak <luckasti.larry@gmail.com>
2023-05-12 12:45:11 +0200

    💄 Set screen height to 100vh

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex max-h-screen flex-col items-center justify-center bg-black">
+    <main className="flex h-screen flex-col items-center justify-center bg-black">
       
       <div className="w-full px-10 lg:max-w-5xl items-center justify-center lg:flex text-white">
         <p className="font-bangers text-5xl lg:text-8xl tracking-wider text-green-500 text-center">COMING SOON</p>
